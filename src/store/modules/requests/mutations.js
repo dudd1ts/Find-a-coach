bd5091f758c3a52ecc91537cb47142d3b9f2c93b
dudd1ts <kserg90@gmail.com>
2022-12-05 21:15:47 +0300

    Add route transitions

--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,8 +1,1 @@
-export default {
-  addRequest(state, payload) {
-    state.requests.push(payload);
-  },
-  setRequests(state, payload) {
-    state.requests = payload;
-  }
-};
+export default {  addRequest(state, payload) {    state.requests.push(payload);  },  setRequests(state, payload) {    state.requests = payload;  }};
